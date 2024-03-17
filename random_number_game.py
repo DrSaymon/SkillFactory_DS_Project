30b95c87d43f8df3721ce3a4a2aff3232ee4fcc7
DrSaymon <impair36@gmail.com>
2024-03-18 00:22:28 +0700

    changed metod of search in function game_core_v3

--- random_number_game.py
+++ random_number_game.py
@@ -1,35 +1,7 @@
 import numpy as np
 
-def game_core_v3(number: int = 1) -> int:
-    """
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
-
-    Returns:
-        int: Число попыток
-    """
-    
-    if number > 100 or number < 1:
-        raise ValueError('Функция принимает только числа в диапазоне от 1 до 100')
-    
-    count = 0
-    min_num = 1
-    max_num = 101
-    predict = np.random.randint(min_num, max_num)
-
-    while number != predict:
-        count += 1
-        if predict > number:
-            max_num = predict
-            predict = np.random.randint(min_num, max_num)
-        elif predict < number:
-            min_num = predict
-            predict = np.random.randint(min_num, max_num)
-
-    return count
-
 def score_game(random_predict) -> int:
-    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
@@ -47,3 +19,33 @@ def score_game(random_predict) -> int:
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
     return score
+
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    
+    if number > 100 or number < 1:
+        raise ValueError('Функция принимает только числа в диапазоне от 1 до 100')
+    
+    count = 0
+    min_num = 0
+    max_num = 100
+    predict = None
+
+    while number != predict:
+        count += 1
+        predict = round((min_num+max_num)/2)
+        if predict > number:
+            max_num = predict
+        else:
+            min_num = predict
+
+    return count
+
+score_game(game_core_v3)
